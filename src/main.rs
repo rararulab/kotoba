a6b3a5229482b549ae3f1e8ea973221e2e768b21
crrow <hahadaxigua@gmail.com>
2026-03-20 23:06:48 +0900

    fix(cli): allow too_many_lines on main (#10)

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ use clap::Parser;
 use cli::{Cli, Command};
 
 #[tokio::main]
+#[allow(clippy::too_many_lines)]
 async fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
     let cli = Cli::parse();
     let db = db::Database::open_default().await?;
